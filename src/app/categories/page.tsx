ae65336e6da1179a4f8b824d889c71957a16068f
Daniel Schmitz <danieljfa@gmail.com>
2024-03-14 07:50:02 -0300

    fetch category

--- src/app/categories/page.tsx
+++ src/app/categories/page.tsx
@@ -1,15 +1,27 @@
+import { Category, PrismaClient } from "@prisma/client";
 import Link from "next/link";
 
+export const getData = async (): Promise<Category[]> => {
+  const prisma = new PrismaClient();
+  return await prisma.category.findMany();
+};
 
+const page = async () => {
+  const data = await getData();
+  return (
+    <div>
+      <ul>
+        {data.map((category) => (
+          <li key={category.id}>
+            <Link href={`/categories/${category.id}`}>{category.name}</Link>
+          </li>
+        ))}
+      </ul>
+      <br />
+      <br />
+      <Link href="/categories/new">create new</Link>
+    </div>
+  );
+};
 
-const page = () => {
-    return (
-        <div>
-            categories list
-            <br/><br/>
-            <Link href="/categories/new">create new</Link>
-        </div>
-    );
-}
-
-export default page
+export default page;
