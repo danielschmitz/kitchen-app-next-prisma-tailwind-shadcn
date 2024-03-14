828e709620f01498025de3c7aa494aef66511264
Daniel Schmitz <danieljfa@gmail.com>
2024-03-14 18:35:59 -0300

    Error com useFormState

---
+++ src/app/categories/edit/[id]/form.tsx
@@ -0,0 +1,38 @@
+"use client";
+
+import { Button } from "@/components/ui/button";
+import { useFormState } from "react-dom";
+
+export default function UpdateForm({ formAction, initialData }: any) {
+  const [formState, action] = useFormState(formAction, {
+    errors: {},
+  });
+
+  return (
+    <form action={action}>
+      <input type="hidden" name="id" defaultValue={initialData.id} />
+      <div className="mb-4">
+        <label
+          className="block text-gray-700 text-sm font-bold mb-2"
+          htmlFor="name"
+        >
+          Name
+        </label>
+        <input
+          className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline min-w-[300px]"
+          id="name"
+          name="name"
+          type="text"
+          placeholder="Category name"
+          defaultValue={initialData.name}
+        />
+        {formState.errors.name && (
+          <p className="text-red-500 text-xs italic">{formState.errors.name}</p>
+        )}
+      </div>
+      <div className="flex items-center justify-between">
+        <Button type="submit">Save</Button>
+      </div>
+    </form>
+  );
+}
--- src/app/categories/edit/[id]/page.tsx
+++ src/app/categories/edit/[id]/page.tsx
@@ -1,25 +1,28 @@
-import { Button } from "@/components/ui/button";
 import { PrismaClient } from "@prisma/client";
-import Link from "next/link";
 import { redirect } from "next/navigation";
-import { number } from "zod";
+import UpdateForm from "./form";
 
 const page = async ({ params }: { params: { id: string } }) => {
-  async function saveCategory(formData: FormData) {
+  async function saveCategory(formState: any, formData: FormData) {
     "use server";
-
-    const formObject = Object.fromEntries(formData);
+    
+    const id = formData.get('id')
+    const name = formData.get('name')
 
     // validate data
-    if (!formObject.name) {
-      throw new Error("Name is required");
+    if (!formData.get('name')) {
+      return {
+        errors: {
+          name: 'Name is required',
+        },
+      };
     }
 
     // mutate data
     const prisma = new PrismaClient();
     await prisma.category.update({
-      where: { id: parseInt(formObject.id as string) },
-      data: { name: formObject.name as string },
+      where: { id: parseInt(id as string) },
+      data: { name: name as string },
     });
 
     redirect("/categories");
@@ -32,30 +35,7 @@ const page = async ({ params }: { params: { id: string } }) => {
   return (
     <div>
       <div className="flex flex-row w-full">
-        <form
-          action={saveCategory}
-        >
-          <input type="hidden" name="id" defaultValue={category.id} />
-          <div className="mb-4">
-            <label
-              className="block text-gray-700 text-sm font-bold mb-2"
-              htmlFor="name"
-            >
-              Name
-            </label>
-            <input
-              className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline min-w-[300px]"
-              id="name"
-              name="name"
-              type="text"
-              placeholder="Category name"
-              defaultValue={category.name}
-            />
-          </div>
-          <div className="flex items-center justify-between">
-            <Button type="submit">Save</Button>
-          </div>
-        </form>
+          <UpdateForm formAction={saveCategory} initialData={category} />
       </div>
     </div>
   );
