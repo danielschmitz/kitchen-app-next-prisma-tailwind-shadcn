b554e21c5f570578059770f558dbd9b460e8c714
Daniel Schmitz <danieljfa@gmail.com>
2024-03-14 07:56:45 -0300

    Card

--- src/app/categories/page.tsx
+++ src/app/categories/page.tsx
@@ -1,6 +1,16 @@
 import { Category, PrismaClient } from "@prisma/client";
 import Link from "next/link";
 
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import { Button } from "@/components/ui/button";
+
 export const getData = async (): Promise<Category[]> => {
   const prisma = new PrismaClient();
   return await prisma.category.findMany();
@@ -10,16 +20,30 @@ const page = async () => {
   const data = await getData();
   return (
     <div>
-      <ul>
-        {data.map((category) => (
-          <li key={category.id}>
-            <Link href={`/categories/${category.id}`}>{category.name}</Link>
-          </li>
-        ))}
-      </ul>
-      <br />
-      <br />
-      <Link href="/categories/new">create new</Link>
+      <Card>
+        <CardHeader>
+          <CardTitle>Categories</CardTitle>
+          <CardDescription>List Categories</CardDescription>
+        </CardHeader>
+        <CardContent>
+          <p>
+            <ul>
+              {data.map((category) => (
+                <li key={category.id}>
+                  <Link href={`/categories/${category.id}`}>
+                    {category.name}
+                  </Link>
+                </li>
+              ))}
+            </ul>
+          </p>
+        </CardContent>
+        <CardFooter>
+          <Link href="/categories/new">
+            <Button>create new</Button>
+          </Link>
+        </CardFooter>
+      </Card>
     </div>
   );
 };
